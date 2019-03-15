4940380313528042506eb74b3bfc225b2b1fb99e
Mohamed772 <Mohamed772@users.noreply.github.com>
2019-03-15 09:47:53 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,17 +1,23 @@
 import pygame
-pygame.display.set_caption("Nom de la fenetre")
-#logo = pygame.image.load("logo.png").convert()
-#pygame.display.set_icon(logo)
+# pygame.display.set_caption("Nom de la fenetre")
+# logo = pygame.image.load("logo.png").convert()
+# pygame.display.set_icon(logo)
 
 pygame.init()
 screen_width=700
 screen_height=400
 screen=pygame.display.set_mode([screen_width,screen_height])
 
-continuer = True
+BLANC = (255, 255, 255)
+BLEU_CIEL = (185, 240, 240)
+ROUGE = (255, 0, 0)
+VERT = (0, 255, 0)
 
+continuer = True
 while continuer:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             continuer = False
+    
+
 pygame.quit()
