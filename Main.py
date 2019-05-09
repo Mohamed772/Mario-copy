87ec9d866b2b8cbcb8d9235cfe8b730043200c82
Mohamed772 <mohamed-siso@hotmail.fr>
2019-05-09 18:50:51 +0200

    up

--- Main.py
+++ Main.py
@@ -1,9 +1,7 @@
 import sys
 from math import pi
 from pygame.locals import *
-import pygame
 from perso import *
-#from perso import *
 
 # pygame.display.set_caption("Nom de la fenetre")
 # logo = pygame.image.load("logo.png").convert()
@@ -23,10 +21,20 @@ BLANC = (255, 255, 255)
 BLEU_CIEL = (185, 240, 240)
 ROUGE = (255, 0, 0)
 VERT = (0, 255, 0)
+#fonctions
+def colision(player_rect,objects):
+    for mur in objects:
+        old_rect = player_rect_old
+        if old_rect.right <= mur.left and player_rect.right > mur.left:
+            player_rect.right = mur.left
+        if old_rect.left >= mur.right and player_rect.left < mur.right:
+            player_rect.left = mur.right
+        if old_rect.bottom <= mur.top and player_rect.bottom > mur.top:
+            player_rect.bottom = mur.top
 
 # Personnages
 player_surface = pygame.image.load("left1.png").convert_alpha()
-player_rect = player_surface.get_rect(center=(200, 100)) 
+player_rect = player_surface.get_rect(center=(200, 100))
 player_y_change = 0 
 player_face=0
 
@@ -36,6 +44,19 @@ comic_font = pygame.font.SysFont("Comi Sans MS", 32)
 Vie = 4
 text_surface = comic_font.render("Vie: {}".format(Vie), True, BLANC)
 
+# murs
+taille_mur = 50
+mur_surf = pygame.Surface((taille_mur,taille_mur))
+mur_surf.fill(BLANC)
+mur0_rect = pygame.Rect(300, 330, taille_mur, taille_mur)
+pygame.draw.rect(mur_surf, (255,255,255), mur0_rect)
+mur1_rect = pygame.Rect(200, 330, taille_mur,taille_mur)
+pygame.draw.rect(mur_surf,(255,255,255), mur1_rect)
+mur2_rect = pygame.Rect(100, 330, taille_mur,taille_mur)
+pygame.draw.rect(mur_surf,(255,255,255), mur2_rect)
+objects = [mur0_rect, mur1_rect,mur2_rect]
+
+jump_counter = True
 jump = False
 continuer = True
 deplacement_left = False
@@ -54,46 +75,53 @@ while continuer:
             deplacement_left = True
         if keys[pygame.K_UP]:
             jump = True
+    
+    player_rect_old = player_rect.copy()
 
     if deplacement_left:
         player_rect.left -= 3
         player_face=(player_face+1)%2
         player_surface=player_left[0+player_face]
-        deplacement_left = False
+    deplacement_left = False
     if deplacement_right:
         player_rect.left += 3
         player_face=(player_face+1)%2
         player_surface=player_right[0+player_face]
         deplacement_right = False
-    if jump: # Sauter
-        player_y_change = -9
+    if jump and jump_counter: # Sauter
+        jump_counter = False
+        player_y_change = -15
     player_y_change += GRAVITY
     player_rect.move_ip(0, player_y_change)
     jump = False
     if player_rect.y < 10: 
      player_rect.y = 10 
      player_y_change = 0 
-    elif player_rect.bottom >= screen_height-20: 
+    if player_rect.bottom >= screen_height-20: 
      player_rect.bottom = screen_height-20 
-     player_y_change = 0 
+     player_y_change = 0
+     jump_counter = True
 
+    colision(player_rect,[mur for mur in objects if player_rect.colliderect(mur)])
+        
     # Fond provisoire
     screen.fill(BLEU_CIEL)
 
     # sol
     pygame.draw.line(screen, ROUGE,(0, screen_height-20),(screen_width, screen_height-20),5)
 
-    # rectangle
-    pygame.draw.rect(screen, ROUGE, pygame.Rect(200, 330, 50, 50))
-
     # arc de cerle pour compteur de vies
     for angle, color in zip((0, pi),(BLANC, VERT)):
         pygame.draw.arc(screen, color, pygame.Rect(600, 30, 80, 80), angle, angle + pi, 5)
     # Texte: VIE
     screen.blit(text_surface,(610, 60))
-    
+   
+
+
     # affichage 
-    pygame.display.flip() 
+    pygame.display.flip()
+    for mur in objects:
+        screen.blit(mur_surf,mur)
     screen.blit(player_surface, player_rect)
     pygame.display.flip() 
     pygame.display.update() 
--- perso.py
+++ perso.py
@@ -6,5 +6,5 @@ pygame.init()
 #screen=pygame.display.set_mode(700,400)
 #pygame.time.Clock() 
 
-
-player_left = [pygame.image.load("left1.png"),pygame.image.load("left2.png"),pygame.image.load("left3.png"),pygame.image.load("left4.png"),pygame.image.load("left5.png")]
+player_left = [pygame.image.load("left1.png"),pygame.image.load("left2.png")]
+player_right = [pygame.image.load("right1.png"),pygame.image.load("right2.png")]
