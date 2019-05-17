1d6fbd574df8be5ed677d6285fb30811edfbddf2
Mohamed772 <mohamed-siso@hotmail.fr>
2019-05-17 10:36:21 +0200

    pics

--- Main.py
+++ Main.py
@@ -15,16 +15,16 @@ def ingame():
     screen_width=700
     screen_height=400
     screen=pygame.display.set_mode([screen_width,screen_height])
-    clock = pygame.time.Clock() 
-    player_vitesse = 0
+    clock = pygame.time.Clock()
 
     BLANC = (255, 255, 255)
     BLEU_CIEL = (185, 240, 240)
     NOIR = (0, 0, 0)
     VERT = (0, 255, 0)
+    vie = 1
     #fonctions
     def nofall(player_rect): # ne tombe pas
-        player_rect.bottom = screen_height-20 
+        player_rect.bottom = screen_height-20
         player_y_change = 0
 
     def colision(player_rect,objects): # fonction de colision
@@ -38,31 +38,44 @@ def ingame():
                 player_rect.bottom = mur.top
 
     # Personnages
-    player_surface = pygame.image.load("left1.png").convert_alpha()
+    player_surface = player_left1
     player_rect = player_surface.get_rect(center=(100, 100))
-    player_y_change = 0 
+    player_y_change = 0
     player_face=0
 
     # Fonts
     comic_font = pygame.font.SysFont("Comi Sans MS", 32)
     comic_font_menu = pygame.font.SysFont("Comi Sans MS", 40)
-    # Vie
-    vie = 1
     jouer_surface = comic_font_menu.render("JOUER", True, VERT)
     rejouer_surface = comic_font_menu.render("REJOUER", True, VERT)
     vie_surface = comic_font.render("Vie: {}".format(vie), True, BLANC)
 
     # murs
     taille_mur = 20
-    mur_surf = pygame.Surface((taille_mur,taille_mur))
+    mur_surf = pygame.Surface((taille_mur,taille_mur+10))
     mur_surf.fill(NOIR)
-    mur0_rect = pygame.Rect(300, 355, taille_mur, taille_mur)
+    mur0_rect = pygame.Rect(300, 350, taille_mur, taille_mur+10)
     pygame.draw.rect(mur_surf, (255,255,255), mur0_rect)
-    mur1_rect = pygame.Rect(200, 355, taille_mur,taille_mur)
+    mur1_rect = pygame.Rect(200, 350, taille_mur,taille_mur+10)
     pygame.draw.rect(mur_surf,(255,255,255), mur1_rect)
-    mur2_rect = pygame.Rect(100, 355, taille_mur,taille_mur)
+    mur2_rect = pygame.Rect(100, 350, taille_mur,taille_mur+10)
     pygame.draw.rect(mur_surf,(255,255,255), mur2_rect)
     objects = [mur0_rect, mur1_rect,mur2_rect]
+
+    # pics
+    taille_pic = 30
+    pic_surf = pygame.image.load("pic.png")
+    pic0_rect = pygame.Rect(350,330,taille_pic,taille_pic)
+    pic1_rect = pygame.Rect(400,330,taille_pic,taille_pic)
+    objects_mal = [pic0_rect,pic1_rect]
+
+    # arrivé
+    arrive_surf= pygame.Surface((5,40))
+    arrive_surf.fill(VERT)
+    arrive_rect = pygame.Rect(670,338, 5, 40)
+    pygame.draw.rect(arrive_surf, (VERT),arrive_rect)
+
+
 
     jump_counter = True
     jump = False
@@ -72,12 +85,12 @@ def ingame():
     menu = True
     mort = False
 
-    
+
 
 
     while menu:
         clock.tick(30)
-        for event in pygame.event.get(): 
+        for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 menu = False
 
@@ -92,7 +105,7 @@ def ingame():
         bouton_surf = pygame.Surface((bouton_width,bouton_height))
         bouton_surf.fill(BLANC)
         bouton_rect = pygame.Rect(bouton_pos_x,bouton_po_y,bouton_width,bouton_height)
-        
+
         clic_gauche = pygame.mouse.get_pressed()[0]
         clic_pos_x = pygame.mouse.get_pos()[0]
         clic_pos_y = pygame.mouse.get_pos()[1]
@@ -104,17 +117,17 @@ def ingame():
         # Fond provisoire
         screen.fill(BLEU_CIEL)
 
-        # affichage 
+        # affichage
         screen.blit(menu_surf, menu_rect)
         screen.blit(bouton_surf,bouton_rect)
         screen.blit(jouer_surface,(305, 235))
-        pygame.display.flip() 
-        pygame.display.update() 
+        pygame.display.flip()
+        pygame.display.update()
 
     pygame.key.set_repeat(1,20)
     while jeu:
         clock.tick(30)
-        for event in pygame.event.get(): 
+        for event in pygame.event.get():
             keys = pygame.key.get_pressed()
             if event.type == pygame.QUIT:
                 jeu = False
@@ -124,7 +137,7 @@ def ingame():
                 deplacement_left = True
             if keys[pygame.K_UP]:
                 jump = True
-        
+
         player_rect_old = player_rect.copy()
 
         if deplacement_left: # gauche
@@ -143,7 +156,7 @@ def ingame():
         player_y_change += GRAVITY
         player_rect.move_ip(0, player_y_change)
         jump = False
-        
+
         if player_rect.bottom >= screen_height-20 and player_rect.left <= 400: #sur le sol
             nofall(player_rect)
             jump_counter = True
@@ -154,13 +167,21 @@ def ingame():
             player_rect= player_surface.get_rect(center=(100, 100))
             vie -= 1
             vie_surface = comic_font.render("Vie: {}".format(vie), True, BLANC)
-            if vie == 0:
-                mort = True
-                jeu = False
 
+
+        if player_rect.colliderect(arrive_rect):
+            ingame()
 
         colision(player_rect,[mur for mur in objects if player_rect.colliderect(mur)])
-            
+
+        for pic in objects_mal:
+            if player_rect.colliderect(pic) == 1:
+                vie -= 1
+                vie_surface = comic_font.render("Vie: {}".format(vie), True, BLANC)
+
+        if vie == 0:
+                mort = True
+                jeu = False
         # Fond provisoire
         screen.fill(BLEU_CIEL)
 
@@ -169,25 +190,28 @@ def ingame():
         pygame.draw.line(screen, NOIR,(450, screen_height-20),(screen_width, screen_height-20),5)
 
         # arc de cerle pour compteur de vies
-        for angle, color in zip((0, pi),(BLANC, VERT)):
+        for angle, color in zip((0, pi),(BLANC, NOIR)):
             pygame.draw.arc(screen, color, pygame.Rect(600, 30, 80, 80), angle, angle + pi, 5)
         # Texte: VIE
         screen.blit(vie_surface,(610, 60))
 
-    
 
 
-        # affichage 
+
+        # affichage
         pygame.display.flip()
+        screen.blit(arrive_surf, arrive_rect)
+        screen.blit(pic_surf, pic0_rect)
+        screen.blit(pic_surf, pic1_rect)
         for mur in objects:
-            screen.blit(mur_surf,mur)
+            screen.blit(mur_surf, mur)
         screen.blit(player_surface, player_rect)
-        pygame.display.flip() 
-        pygame.display.update() 
+        pygame.display.flip()
+        pygame.display.update()
 
     while mort:
         clock.tick(30)
-        for event in pygame.event.get(): 
+        for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 menu = False
 
@@ -202,7 +226,7 @@ def ingame():
         bouton_surf = pygame.Surface((bouton_width,bouton_height))
         bouton_surf.fill(BLANC)
         bouton_rect = pygame.Rect(bouton_pos_x,bouton_po_y,bouton_width,bouton_height)
-        
+
         clic_gauche = pygame.mouse.get_pressed()[0]
         clic_pos_x = pygame.mouse.get_pos()[0]
         clic_pos_y = pygame.mouse.get_pos()[1]
@@ -213,12 +237,12 @@ def ingame():
         # Fond provisoire
         screen.fill(BLEU_CIEL)
 
-        # affichage 
+        # affichage
         screen.blit(menu_surf, menu_rect)
         screen.blit(bouton_surf,bouton_rect)
         screen.blit(rejouer_surface,(290, 235))
-        pygame.display.flip() 
-        pygame.display.update() 
+        pygame.display.flip()
+        pygame.display.update()
     pygame.quit()
 
 ingame()
--- perso.py
+++ perso.py
@@ -1,10 +1,14 @@
-import pygame 
-
+import pygame
 pygame.init()
 
 
 #screen=pygame.display.set_mode(700,400)
-#pygame.time.Clock() 
+#pygame.time.Clock()
 
-player_left = [pygame.image.load("left1.png"),pygame.image.load("left2.png")]
-player_right = [pygame.image.load("right1.png"),pygame.image.load("right2.png")]
+player_left1 = pygame.transform.scale(pygame.image.load("left1.png"),(25,25))
+player_left2 = pygame.transform.scale(pygame.image.load("left2.png"),(25,25))
+player_right2= pygame.transform.scale(pygame.image.load("right2.png"),(25,25))
+player_right1 = pygame.transform.scale(pygame.image.load("right1.png"),(25,25))
+player_left = [player_left1,player_left2]
+player_right = [player_right1,player_right2]
+
