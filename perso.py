2c0fc05ec690b9c428cb108d1aac2b05a75ba889
clmng <49289649+clmng@users.noreply.github.com>
2019-05-19 15:47:09 +0200

    musique

--- perso.py
+++ perso.py
@@ -12,3 +12,8 @@ player_right1 = pygame.transform.scale(pygame.image.load("right1.png"),(25,25))
 player_left = [player_left1,player_left2]
 player_right = [player_right1,player_right2]
 
+#music
+pygame.mixer.music.load('game_music.mp3')
+pygame.mixer.music.play()
+pygame.mixer.music.set_volume(0.1)
+
